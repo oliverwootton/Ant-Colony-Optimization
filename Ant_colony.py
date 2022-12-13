087b05d80c6b37669d4a318c1128185c360cc129
Oliver Wootton <75435448+onw201@users.noreply.github.com>
2022-12-13 14:57:44 +0000

    commit

--- Ant_colony.py
+++ Ant_colony.py
@@ -14,24 +14,22 @@ def set_zeros(column, n, H):
                 H[i][j] = 0
     return H
 
-def transition_prob(i, n, T, H):
+def transition_prob(i, n, T, route):
     den = 0 
     N = [None] * n
     for j in range(n):
-        N[j] = T[i][j] * H[i][j]
-        print("n")
-        print(T[i][j])
-        print(H[i])
-        print(T[i][j] * H[i][j])
-        den += N[j]
-    print(den)
+        if (i != j) and (j not in route):
+            N[j] = T[i][j]
+            den += N[j]
+        else:
+            N[j] = 0 
     for j in range(n):
-        if den > 0:
+        if den > 0 and N[j] != 0:
             N[j] = N[j]/den
         else:
             N[j] = 0
     return N
-    
+
 def cumulative_prop(num_cities, N):
     CP = 0
     ran = random.random()
@@ -39,7 +37,6 @@ def cumulative_prop(num_cities, N):
     for i in range(num_cities):
         CP += N[i]
         if CP > ran:
-            # print(f"City number --> {i+1}")
             return i+1
 
 def evaportation(rho, T, n):
@@ -68,9 +65,9 @@ def fitness(routes, H, m):
             x = y
         fitness.append(delta)
     return fitness
-  
+
 def best_fitness(delta):
-    min = delta[0]*3
+    min = delta[0]
     for fitness in delta:
         if fitness < min:
             min = fitness
@@ -83,12 +80,8 @@ def one_ant(n, H, T):
     
     for i in range(n-1):
         H = set_zeros(route, n, H)
-        if i == 48:
-            print('\n'.join([''.join(['{:20}'.format(item) for item in row]) for row in H]))
         # Calculates the probability of moving to the next building
-        N = transition_prob(route[-1], n, T, H)
-        print(route[-1])
-        # print(N)
+        N = transition_prob(route[-1], n, T, route)
         # Randomly goes to the next building
         buildingNo = cumulative_prop(n, N)
         route.append(buildingNo-1)
@@ -96,33 +89,30 @@ def one_ant(n, H, T):
     return route  
 
 def main(n, D, F, m, rho):
-    T = pheromone(n)
     fitness_eval = 10000
-    
+    T = pheromone(n)
+    fitness = []
     for i in range((int) (fitness_eval/m)):
         ant_paths = []
-        # Generates a list of m ant paths
-        k = 0 
-        for _ in range(m):
+        
+        for j in range(m):
+            # Generates a list of m ant paths
             H = cost_function(D, F)
             ant_paths.append(one_ant(n, H, T))
-            print(k)
-            k += 1
         
         H = cost_function(D, F)
-        delta = fitness(ant_paths, H, m)
+        delta = fitness(ant_paths, H, m) 
         
         j = 0
         for route in ant_paths:
-            T = ant_pheromone(route, 1/delta[i], T, n)
+            T = ant_pheromone(route, 1/delta[j], T, n)
             j += 1
         
         T = evaportation(rho, T, n)
         
-        minimum_fitness = best_fitness(delta)
-        print(minimum_fitness)
-        
-    
+        fitness.append(delta)
+    print(best_fitness(fitness))
+
 random.seed(0)
 m = 100
 e = 0.9
@@ -130,8 +120,3 @@ e = 0.9
 n, D, F = file_read('data.txt')
 
 main(n, D, F, m, e)
-
-# for i in range(1):
-#     T = ant_colony(D, F, n, num_ants, alpha, beta)
-#     # print('\n'.join([''.join(['{:20}'.format(item) for item in row]) for row in T]))
-#     print(route_finder(T))
